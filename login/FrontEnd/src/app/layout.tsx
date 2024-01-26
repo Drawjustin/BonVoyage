5757d089d4f6eb6d81c5b8c86f199f3afd71f22d
bluebear9508@naver.com <bluebear9508@naver.com>
2024-01-26 16:45:22 +0900

    build: first upload for Jenkins

--- login/FrontEnd/src/app/layout.tsx
+++ login/FrontEnd/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-          <Navbar currentUser={currentUser}/>
+          {/* <Navbar currentUser={currentUser}/> */}
           <ToastProvider />
             {children}
       </body>
