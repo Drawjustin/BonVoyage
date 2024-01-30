bd432f8e065da394060f86f1c3ad1fdb6d62fdb3
DH <alexander930828@gmail.com>
2024-01-30 14:28:21 +0900

    fix : SpringRepo
    Former-commit-id: 574c77887703c31435ed30303b42358bb68dc917

--- login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/domain/Artist.java
+++ login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/domain/Artist.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02a2f3994197ddc78bde1844ad9073964ad5cadf85daf7528de3333427562c7c
-size 1833
+oid sha256:8cc04bda462f6009f77277f2b86d21c3af75f8bc64f4aed86665e1ecd5cbad0d
+size 1802
