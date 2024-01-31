c286397d0147962db0187d52f60b32c610f7a463
DH <alexander930828@gmail.com>
2024-01-31 10:48:54 +0900

    ++++
    Former-commit-id: c5b7529d0354a8458024c0f52609b4cf3eb1691a

--- login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/Controller/ArtistController.java
+++ login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/Controller/ArtistController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f253c9d1ae370bd5eeb95fce06c7b2d0a50114633dcd44d25635bfa5227718ed
-size 2089
+oid sha256:a0d6b22a4594ed5e0aa304ca51991ec501950a0c72994ea4d9420c8dda04eff4
+size 2080
