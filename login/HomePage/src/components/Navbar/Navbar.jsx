524e3b6aaf992aa4c51f8d784cb0176d46cf311d
sueminpark <breathin_suemin@naver.com>
2024-01-31 14:04:52 +0900

    S10P12A207-240 : HomePage Navbar css change

--- login/HomePage/src/components/Navbar/Navbar.jsx
+++ login/HomePage/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.scss';
-import Image from 'next/image'
+import Image from 'next/image';
+import { ButtonContainer } from './ButtonContainer';
+
 
 const Navbar = () => {
   return (
@@ -11,8 +13,7 @@ const Navbar = () => {
         <Image src='/assets/images/logo.png' className={styles.logo} width={0.3} height={0.3}/>
       </Link>
       <ul>
-        <Link href='/LoginPage'>로그인</Link>
-        <Link href='/SignupPage'>회원가입</Link>
+        <ButtonContainer />
       </ul>
     </nav>
   );
