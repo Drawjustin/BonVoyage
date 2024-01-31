e77b907be4f500bf6cfef07fc2979ca8392e6d89
DH <alexander930828@gmail.com>
2024-01-31 13:23:28 +0900

    test_Backend
    Former-commit-id: 8440eb4d9e160ae40a160344f41c4ed9730feecc

--- login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/service/ArtistService.java
+++ login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/service/ArtistService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a51ad4be2cd917072b1b731136b14da0ed644b0c4e483973615bbd6aba40a316
-size 2733
+oid sha256:a17b040e7142e2305e1a5606df804c42265e716f7a1c2bb35a9bc7f1d4175fd4
+size 2788
