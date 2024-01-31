8cd81499fdac38fc25e0adf722169e87a57657a0
DH <alexander930828@gmail.com>
2024-01-31 14:08:15 +0900

    fix bug
    Former-commit-id: aed649ed25075093f925aba80cf2dd74ac2e1849

--- login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/repository/ArtistRepository.java
+++ login/BackEnd/ArtBridge-login/src/main/java/ArtBridge/ArtBridgelogin/repository/ArtistRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6289c7c3db365f03833844e94572da3f01f73af6ae47c6edfcd813eba5875255
+oid sha256:381136d4a9b59aefd15b7f038903faecf01f6c77543377a497d3b57be9c08a56
 size 1766
