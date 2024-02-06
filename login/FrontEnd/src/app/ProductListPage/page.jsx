70b1e2e9514e3102cf52e93b990e8c24649392e6
bluebear9508@naver.com <bluebear9508@naver.com>
2024-02-06 09:07:57 +0900

    S10P12A207-289 hotfix: Fixed Build Error

--- login/FrontEnd/src/app/ProductListPage/page.jsx
+++ login/FrontEnd/src/app/ProductListPage/page.jsx
@@ -4,15 +4,16 @@ import { Provider } from 'react-redux';
 import store from '@/store';
 import Categories from '@/components/categories/Categories/Categories';
 import axios from 'axios';
+import Navbar from '@/components/Navbar/Navbar';
 
 export default async function ProductListPage () {
 
-  try{
-    const response = await axios.get('https://i10a207.p.ssafy.io/api/item');
-    //console.log(response.data);
-  } catch (error) {
-    console.log("에러", error);
-  }
+  // try{
+  //   const response = await axios.get('https://i10a207.p.ssafy.io/api/item');
+  //   console.log(response.data);
+  // } catch (error) {
+  //   console.log("에러", error);
+  // }
   
   
 
