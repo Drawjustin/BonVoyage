6d72706a8b10ea6e7b7e896c5e16b1c9ae62adf6
bluebear9508@naver.com <bluebear9508@naver.com>
2024-02-07 09:50:08 +0900

    S10P12A207 feat: Modified ProductInfo (deleted category)

--- login/FrontEnd/src/components/products/ProductInfo/ProductInfo.tsx
+++ login/FrontEnd/src/components/products/ProductInfo/ProductInfo.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Avatar from '../../Avatar';
-import ProductCategory from '../ProductCategory/ProductCategory';
 import { formatTime } from '@/helpers/dayjs';
 import styles from './ProductInfo.module.scss';
 
 const ProductInfo = ({
   user,
-  category,
   createdAt,
   description
 }: any) => {
@@ -21,14 +19,6 @@ const ProductInfo = ({
           {formatTime(createdAt)}
         </div>
       </div>
-      <hr />
-      {category &&
-        <ProductCategory
-          icon={category.icon}
-          label={category.label}
-          description={category.description}
-        />
-      }
       <hr />
       <div className={styles.description}>
         {description}
