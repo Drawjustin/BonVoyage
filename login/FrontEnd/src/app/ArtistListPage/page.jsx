1cbca2d93c52d61034d37465df5bb0b47299efa8
WookeyKim95 <bluebear9508@naver.com>
2024-02-09 21:45:14 +0900

    S10P12A207 : build: Modified to make a axios connection for ArtistPage

--- login/FrontEnd/src/app/ArtistListPage/page.jsx
+++ login/FrontEnd/src/app/ArtistListPage/page.jsx
@@ -5,12 +5,13 @@ import Navbar from '../../components/Navbar/Navbar';
 import axios from 'axios';
 import styles from './ArtistListPage.module.scss'
 import getCurrentUser from '@/app/actions/getCurrentUser';
+import ArtistCard from '@/components/Artists/ArtistCard/ArtistCard';
 
 const ArtistListPage = async () => {
 
   const currentUser = await getCurrentUser();
-  const Artists = {} // await axios.get('https://i10a207.p.ssafy.io/api/artists');
-    // console.log(Artists.data);
+  const Artists = await axios.get('https://i10a207.p.ssafy.io/api/artists');
+  console.log(Artists.data);
 
   return (
     <div style={{ width: '1200px' }}>
@@ -21,10 +22,10 @@ const ArtistListPage = async () => {
 
       <div className={styles.grid}>
               {Artists.data?.map((Artist) =>
-                  <ProductCard
-                    currentUser={'퍄퍄퍄'}
-                    key={Artists.itemSeq}
-                    data={product}
+                  <ArtistCard
+                    currentUser={currentUser}
+                    key={Artist.itemSeq}
+                    data={Artist}
                   />)}
               </div>
     </div>
