fb3fd6efe31b1193e2a22107fb5cedf033d99cab
WookeyKim95 <bluebear9508@naver.com>
2024-02-12 00:17:13 +0900

    S10P12A207 : !HOTFIX: Trial to fix login Error

--- login/FrontEnd/src/app/api/auth/[...nextauth]/route.ts
+++ login/FrontEnd/src/app/api/auth/[...nextauth]/route.ts
@@ -65,7 +65,6 @@ const authOptions:NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   jwt: {
     maxAge: 30 * 24 * 60 * 60, // 30days
-    secret: process.env.NEXT_PUBLIC_JWT_SECRET
   },
   pages: {
     signIn: '/LoginPage',
