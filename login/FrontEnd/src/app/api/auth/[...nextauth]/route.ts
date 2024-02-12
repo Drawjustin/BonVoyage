c5582969e64ead7d2b382753f71b13e17e67343a
WookeyKim95 <bluebear9508@naver.com>
2024-02-12 19:40:20 +0900

    S10P12A207: !HOTFIX: Trial to Fix Login Error

--- login/FrontEnd/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import axios, { AxiosResponse } from "axios";
-import { authOptions } from "@/utils/authOptions";
-
-const handler = NextAuth(authOptions);
-
-export {handler as GET, handler as POST};
