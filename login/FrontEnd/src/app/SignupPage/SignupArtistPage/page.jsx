c9e28b5fc8493b98b93e0adbbb64aecd2e139aba
jjeong41 <yj_w41l@naver.com>
2024-02-13 10:33:28 +0900

    S10P12A207-292 feat: axios form & add css

--- login/FrontEnd/src/app/SignupPage/SignupArtistPage/page.jsx
+++ login/FrontEnd/src/app/SignupPage/SignupArtistPage/page.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Form from '../../../components/Signup/ArtistInputSignup/ArtistInputSignup'
-import Navbar from '../../../components/Navbar/Navbar';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 const SignupArtistPage = async () => {
   const currentUser = await getCurrentUser();
   return (
       <div>
-      <Navbar/>
         <Form />
       </div>
   )
